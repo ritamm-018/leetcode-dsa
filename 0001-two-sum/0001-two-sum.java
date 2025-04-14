11671ae04a649784da8b6a21b917a7d838195fb3
ritamm-018 <ritammdutta2005@gmail.com>
2025-04-15 01:14:37 +0530

    Time: 4 ms (58.4%), Space: 44.9 MB (63.53%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,47 @@
+import java.util.HashMap;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // HashMap to store the value and its corresponding index
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        // Iterate through the array
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            
+            // Check if the complement is already in the map
+            if (map.containsKey(complement)) {
+                // Return the indices of the two numbers
+                return new int[] { map.get(complement), i };
+            }
+            
+            // Otherwise, add the current number and its index to the map
+            map.put(nums[i], i);
+        }
+        
+        // If no solution is found, although we are guaranteed to have one, return an empty array
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        // Example 1
+        int[] nums1 = {2, 7, 11, 15};
+        int target1 = 9;
+        int[] result1 = solution.twoSum(nums1, target1);
+        System.out.println("Indices: [" + result1[0] + ", " + result1[1] + "]");
+        
+        // Example 2
+        int[] nums2 = {3, 2, 4};
+        int target2 = 6;
+        int[] result2 = solution.twoSum(nums2, target2);
+        System.out.println("Indices: [" + result2[0] + ", " + result2[1] + "]");
+        
+        // Example 3
+        int[] nums3 = {3, 3};
+        int target3 = 6;
+        int[] result3 = solution.twoSum(nums3, target3);
+        System.out.println("Indices: [" + result3[0] + ", " + result3[1] + "]");
+    }
+}
