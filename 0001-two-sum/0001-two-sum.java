1195cbb1e45d69d587bc767fb4fd1dac14738121
ritamm-018 <ritammdutta2005@gmail.com>
2025-06-12 00:32:17 +0530

    Update 0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -2,24 +2,19 @@ import java.util.HashMap;
 
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // HashMap to store the value and its corresponding index
+        
         HashMap<Integer, Integer> map = new HashMap<>();
         
-        // Iterate through the array
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             
-            // Check if the complement is already in the map
             if (map.containsKey(complement)) {
-                // Return the indices of the two numbers
                 return new int[] { map.get(complement), i };
             }
             
-            // Otherwise, add the current number and its index to the map
             map.put(nums[i], i);
         }
         
-        // If no solution is found, although we are guaranteed to have one, return an empty array
         return new int[] {};
     }
 
