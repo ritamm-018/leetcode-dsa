bb5c34bf42ebf9c6c2044912860ba7f2be4449a1
ritamm-018 <ritammdutta2005@gmail.com>
2025-06-12 00:28:06 +0530

    Update 0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -23,25 +23,4 @@ public class Solution {
         return new int[] {};
     }
 
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        
-        // Example 1
-        int[] nums1 = {2, 7, 11, 15};
-        int target1 = 9;
-        int[] result1 = solution.twoSum(nums1, target1);
-        System.out.println("Indices: [" + result1[0] + ", " + result1[1] + "]");
-        
-        // Example 2
-        int[] nums2 = {3, 2, 4};
-        int target2 = 6;
-        int[] result2 = solution.twoSum(nums2, target2);
-        System.out.println("Indices: [" + result2[0] + ", " + result2[1] + "]");
-        
-        // Example 3
-        int[] nums3 = {3, 3};
-        int target3 = 6;
-        int[] result3 = solution.twoSum(nums3, target3);
-        System.out.println("Indices: [" + result3[0] + ", " + result3[1] + "]");
-    }
-}
+    
