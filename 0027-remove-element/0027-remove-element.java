70fda6a8e8b860da5f50789c71cfa4439f45bc54
ritamm-018 <ritammdutta2005@gmail.com>
2025-07-03 00:12:36 +0530

    Time: 0 ms (100%), Space: 41.9 MB (67.01%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,29 +1,13 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int k = 0; // Count of elements not equal to val
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[k] = nums[i];
-                k++;
+        int write = 0;
+        for(int read = 0; read < nums.length; read++){
+            if(nums[read] != val){
+                nums[write] = nums[read];
+                write++;
             }
+
         }
-
-        return k;
-    }
-
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-
-        int[] nums = {3, 2, 2, 3};
-        int val = 3;
-
-        int k = sol.removeElement(nums, val);
-
-        System.out.println("k = " + k);
-        System.out.print("Updated nums: ");
-        for (int i = 0; i < k; i++) {
-            System.out.print(nums[i] + " ");
-        }
+        return write;
     }
 }
