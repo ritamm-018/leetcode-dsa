2450778323c8bf9673037a5d205a29c0298c28c2
ritamm-018 <ritammdutta2005@gmail.com>
2026-02-08 00:38:06 +0530

    Time: 4 ms (99.47%), Space: 47.7 MB (83.21%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,25 +1,28 @@
 class Solution {
-    public boolean isPalindrome(String s, int start, int end){
-        while(start < end){
-            if(s.charAt(start) != s.charAt(end)){
+    public boolean samplePalindrome(String s, int i, int j){
+        while(i <= j){
+            if(s.charAt(i) != s.charAt(j)){
                 return false;
             }
-            start++;
-            end--;
+            else{
+                i++;
+                j--;
+            }
+
         }
         return true;
     }
     public boolean validPalindrome(String s) {
-        int start = 0;
-        int end = s.length() - 1;
+        int i = 0, j = s.length() - 1;
 
-        while(start < end){
-            if(s.charAt(start) != s.charAt(end)){
-                return isPalindrome(s, start + 1, end) || isPalindrome(s, start, end - 1);
-
+        while(i <= j){
+            if(s.charAt(i) != s.charAt(j)){
+                return samplePalindrome(s, i + 1, j) || samplePalindrome(s, i, j - 1);
             }
-            start++;
-            end--;
+            else{
+                i++;
+                j--;
+            }
         }
         return true;
     }
